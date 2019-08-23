e92a465d216bb441c7546707097aa5dc24634040
Lili <iliyanastefanova@msn.com>
2019-08-23 17:19:24 -0400

    trying again

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -6,11 +6,9 @@ if($_POST["submit"]) {
     $sender=$_POST["name"];
     $senderEmail=$_POST["email"];
     $message=$_POST["message"];
-
     $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
-
     mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
-
+    
     $thankYou="<p>Thank you! Your message has been sent.</p>";
 }
 
